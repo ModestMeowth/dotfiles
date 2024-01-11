a0c81ff5e629d1ce2e552c121ba34f99d43f87f8
Daniel Eberle <dan.eberle@protonmail.com>
2024-01-11 16:30:07 -0600

    :art: re-jig for install via nix flake?

--- home/hosts/telescreen/default.nix
+++ home/hosts/telescreen/default.nix
@@ -21,9 +21,19 @@
         home-manager.enable = true;
     };
 
+    dconf.settings = {
+      "org/virt-manager/virt-manager/connections" = {
+        autoconnect = [
+          "qemu+ssh://mm@pwnyboy/system"
+        ];
+        uris = [
+          "qemu+ssh://mm@pwnyboy/system"
+        ];
+      };
+    };
+
     imports = [
         ../../shell/git
         ../../shell/nix-direnv
-	../../apps/neovim
     ];
 }
