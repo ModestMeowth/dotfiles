50bc1d630d389e081acc220202d6e5b8092f6c9a
Daniel Eberle <dan.eberle@protonmail.com>
2024-01-13 04:05:31 -0600

    :sparkles: added nixos host config

---
+++ dot_config/home-manager/hosts/nixos/default.nix
@@ -0,0 +1,28 @@
+{ pkgs, config, ... }: {
+    home = {
+        username = "mm";
+        homeDirectory = "/home/mm";
+
+        stateVersion = "23.11";
+
+        packages = with pkgs; [
+            just
+            chezmoi
+            ripgrep
+            eza
+            fzf
+            zoxide
+            zellij
+            starship
+        ];
+    };
+
+    programs = {
+        home-manager.enable = true;
+    };
+
+    imports = [
+        ../../shell/git
+        ../../shell/nix-direnv
+    ];
+}
