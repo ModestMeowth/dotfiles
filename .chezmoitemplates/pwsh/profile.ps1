d7a2e4747a3944db116ea661ce2bc0013cb4de4d
Daniel Eberle <daniel.j.eberle@gmail.com>
2024-03-05 22:56:37 -0600

    modify pwsh cd command add powershell aliases

--- .chezmoitemplates/pwsh/profile.ps1
+++ .chezmoitemplates/pwsh/profile.ps1
@@ -1,3 +1,39 @@
+$Env:DIRENV_CONFIG = "$Env:LOCALAPPDATA\direnv"
+$Env:XDG_CACHE_HOME = "$Env:USERPROFILE\.local\cache"
+$Env:XDG_DATA_HOME = "$Env:USERPROFILE\.local\share"
+
+Set-Alias -Name vi -Value nvim
+Set-Alias -Name vim -Value nvim
+
+function ca { chezmoi apply }
+function cu { chezmoi update }
+function cc { chezmoi cd }
+function ce { chezmoi edit }
+
+function ga { git add -A }
+function cg { git commit }
+function gs { git status }
+function gp { git push }
+function gl { git log }
+function gd { git diff }
+function gds { git diff --staged }
+function gr { git restore . }
+function grs { git restore --staged . }
+
+function cga { chezmoi git add -- -A }
+function cgc { chezmoi git commit }
+function cgs { chezmoi git status }
+function cgp { chezmoi git push }
+function cgl { chezmoi git log }
+function cgd { chezmoi git diff }
+function cgds { chezmoi git diff -- --staged }
+function cgr { chezmoi git restore -- . }
+function cgrs { chezmoi git restore -- --staged . }
+
+function tss { tailscale status }
+function tsp { tailcale ping }
+function tsn { tailscale netcheck }
+
 function bw {
     if(-not (Test-Path env:BW_SESSION) -or ($args[0] -eq "unlock")) {
         $env:BW_SESSION=(bw.exe unlock --raw)
@@ -7,8 +43,5 @@ function bw {
         Invoke-Expression ("bw.exe " + $args)
     }
 }
-
-Set-Alias -Name vi -Value nvim
-Set-Alias -Name vim -Value nvim
 
 Invoke-Expression (&starship init powershell)
