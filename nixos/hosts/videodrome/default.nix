300c69e935f80db38d4d82996d977aa6be4b949b
Daniel Eberle <dan.eberle@protonmail.com>
2024-04-03 01:54:56 -0500

    :wrench: fix tailscale dns-fight

--- nixos/hosts/videodrome/default.nix
+++ nixos/hosts/videodrome/default.nix
@@ -3,4 +3,22 @@
     ./hardware.nix
     ../../modules/wsl.nix
   ];
+
+  wsl.wslConf = {
+    network = {
+      hostname = "videodrome";
+      generateHosts = false;
+      generateResolvConf = false;
+    };
+    user.default = "mm";
+  };
+
+  networking = {
+    nameservers = [
+      "100.100.100.100"
+    ];
+    search = [
+      "cat-alkaline.ts.net"
+    ];
+  };
 }
