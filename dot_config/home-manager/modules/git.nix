67bd9ce6f5a275e2914427f929bdc444b9b0905a
Daniel Eberle <dan.eberle@protonmail.com>
2024-04-03 04:51:51 -0500

    :wrench: pull git configs out of nix

--- dot_config/home-manager/modules/git.nix
+++ dot_config/home-manager/modules/git.nix
@@ -1,56 +1,8 @@
 {pkgs, ...}: {
-  programs = {
-    git = {
-      enable = true;
-
-      userName = "Daniel Eberle";
-      userEmail = "dan.eberle@protonmail.com";
-
-      extraConfig = {
-        init = {
-          defaultBranch = "main";
-        };
-
-        core = {
-          editor = "vim.nox";
-        };
-
-        color = {
-          ui = "auto";
-
-          branch = {
-            current = "yellow reverse";
-            local = "yellow";
-            remote = "green";
-          };
-
-          diff = {
-            meta = "yellow bold";
-            frag = "magenta bold";
-            old = "red";
-            new = "green";
-          };
-
-          status = {
-            added = "yellow";
-            changed = "green";
-            untracked = "cyan";
-          };
-        };
-      };
-    };
-
-    gh = {
-      enable = true;
-
-      settings = {
-        editor = "vim.nox";
-      };
-    };
-  };
-
   home = {
     packages = with pkgs; [
+      git
+      gh
       pre-commit
       gitlint
       shellcheck
