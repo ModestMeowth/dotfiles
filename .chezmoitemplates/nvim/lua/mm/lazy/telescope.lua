ca54167b5fa4504ad19ab26ab1620f665269f0d5
Daniel Eberle <dan.eberle@protonmail.com>
2024-05-07 17:06:38 -0500

    :fire: remove chezmoi nvim plugin

--- .chezmoitemplates/nvim/lua/mm/lazy/telescope.lua
+++ .chezmoitemplates/nvim/lua/mm/lazy/telescope.lua
@@ -8,12 +8,9 @@ return {
         local telescope = require("telescope")
         telescope.setup({})
 
-        telescope.load_extension("chezmoi")
-
         local builtin = require("telescope.builtin")
 
         vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-        vim.keymap.set("n", "<leader>cz", telescope.extensions.chezmoi.find_files, {})
         vim.keymap.set("n", "<C-p>", builtin.git_files, {})
         vim.keymap.set("n", "<leader>pws", function()
             local word = vim.fn.expand("<cword>")
