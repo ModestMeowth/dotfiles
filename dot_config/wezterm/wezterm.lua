6081daddd63edb0ca57b040de6f605849dd266bb
Daniel Eberle <dan.eberle@protonmail.com>
2024-05-08 18:46:00 -0500

    :sparkles: add wezterm config

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+local wt = require"wezterm"
+local config = {}
+if wt.config_builder then
+    config = wt.config_builder()
+end
+
+local wayland_gnome = require"wayland_gnome"
+wayland_gnome.apply_to_config(config)
+
+config.color_scheme = "Dracula (Official)"
+
+config.font_size = 11.0
+config.font = wt.font {
+    family = "NotoSansM Nerd Font Mono",
+    weight = "DemiBold"
+}
+
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+
+return config
