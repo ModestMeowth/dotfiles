4bdeb5f071e74d83939e0b890b5d2c7ad5cf7ea7
Daniel Eberle <daniel.j.eberle@protonmail.com>
2024-05-17 12:03:05 -0500

    massive cleanup

--- dot_config/home-manager/modules/fish/plugins/starship.nix
+++
@@ -1,8 +0,0 @@
-{inputs, pkgs, ...}: {
-  programs.starship = {
-    enable = true;
-    enableTransience = true;
-  };
-
-  xdg.configFile."starship.toml".source = "${inputs.dotfiles}/dot_config/starship.toml.tmpl";
-}
--- nixos/modules/networking.nix
+++
@@ -1,7 +0,0 @@
-{
-  networking = {
-    hostId = "00bab10c";
-    networkmanager.enable = true;
-  };
-  systemd.services.NetworkManager-wait-online.enable = false;
-}
