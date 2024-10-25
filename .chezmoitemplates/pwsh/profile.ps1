8fcccc39ffffdc23cd9e1737cc8ea05d835edc6e
Daniel Eberle <dan.eberle@protonmail.com>
2024-10-25 17:02:38 -0500

    adjust pwsh template

--- .chezmoitemplates/pwsh/profile.ps1
+++ .chezmoitemplates/pwsh/profile.ps1
@@ -2,51 +2,36 @@ $Env:DIRENV_CONFIG = "$Env:LOCALAPPDATA\direnv"
 $Env:XDG_CACHE_HOME = "$Env:USERPROFILE\.local\cache"
 $Env:XDG_DATA_HOME = "$Env:USERPROFILE\.local\share"
 
-Set-Alias -Name vi -Value nvim
-Set-Alias -Name vim -Value nvim
-
-function ca { chezmoi apply }
-function cu { chezmoi update }
-function cc { chezmoi cd }
-function ce { chezmoi edit }
-
-function ga { git add -A }
-function cg { git commit }
-function gs { git status }
-function gp { git push }
-function gl { git log }
-function gd { git diff }
-function gds { git diff --staged }
-function gr { git restore . }
-function grs { git restore --staged . }
-
-function cga { chezmoi git add -- -A }
-function cgc { chezmoi git commit }
-function cgs { chezmoi git status }
-function cgp { chezmoi git push }
-function cgl { chezmoi git log }
-function cgd { chezmoi git diff }
-function cgds { chezmoi git diff -- --staged }
-function cgr { chezmoi git restore -- . }
-function cgrs { chezmoi git restore -- --staged . }
-
-function tss { tailscale status }
-function tsp { tailcale ping }
-function tsn { tailscale netcheck }
-
-function bw {
-    if(-not (Test-Path env:BW_SESSION) -or ($args[0] -eq "unlock")) {
-        $env:BW_SESSION=(bw.exe unlock --raw)
-    }
-
-    if(-not ($args[0] -eq "unlock")) {
-        Invoke-Expression ("bw.exe " + $args)
-    }
-}
-
-Invoke-Expression (&starship init powershell)
-
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
     Import-Module "$ChocolateyProfile"
 }
+
+function which ($command) {
+    Get-Command -Name $command -ErrorAction SilentlyContinue |
+        Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+function la { Get-ChildItem -Force }
+
+function justup {
+    winget pin remove -e Casey.Just
+    winget upgrade -e Casey.Just
+    winget pin add -e Casey.Just
+}
+
+Set-Alias -Name ls -Value la -Options AllScope -Scope Global -ErrorAction SilentlyContinue -Force
+Set-Alias -Name ll -Value la -Options AllScope -Scope Global -ErrorAction SilentlyContinue -Force
+
+Set-Alias -Name cd -Value __zoxide_z -Option AllScope -Scope Global -Force
+Set-Alias -Name ci -Value __zoxide_zi -Option AllScope -Scope Global -Force
+
+Set-Alias -Name grep -Value findstr
+
+Set-Alias -Name vi -Value nvim
+Set-Alias -Name vim -Value nvim
+
+Set-Alias -Name j -Value just
+
+Invoke-Expression (&starship init powershell)
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
